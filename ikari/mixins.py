3ae2625f5a7f7125f56390b7322e4d460777ce13
Zenobius Jiricek <airtonix@gmail.com>
2013-08-02 13:29:28 +0930

    pep8 updates

--- ikari/mixins.py
+++ ikari/mixins.py
@@ -1,18 +1,12 @@
-import re
 import logging
 
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.contrib.sites.models import Site
-from django.core.mail import mail_managers
-from django.core.urlresolvers import reverse
-from django.forms.widgets import HiddenInput
-from django.http import HttpResponseRedirect, HttpResponseForbidden
-from django.views.generic import View, DetailView, ListView, TemplateView, CreateView, UpdateView, DeleteView, RedirectView, FormView
+from django.views.generic import View
 from django.utils.decorators import method_decorator
 
 from . import models
 from . import settings
+
 
 logger = logging.getLogger(__name__)
 logger.addHandler(settings.null_handler)
@@ -20,10 +14,6 @@ logger.addHandler(settings.null_handler)
 
 class DomainViewMixin(object):
     model = models.Domain
-
-    # def get_object(self, *args, **kwargs):
-    #     # We edit current user's domain
-    #     return self.model.objects.get(owner=self.request.user)
 
 
 class ProtectedView(View):
