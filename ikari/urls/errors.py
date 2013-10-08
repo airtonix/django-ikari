f33dc8da2322627d17ab16187d03b3d16a38e432
Zenobius Jiricek <airtonix@gmail.com>
2013-10-09 02:08:00 +1030

    Update error template urls, view and response to allow for project level override

--- ikari/urls/errors.py
+++ ikari/urls/errors.py
@@ -1,43 +1,33 @@
 from django.conf.urls.defaults import *
-from django.utils.translation import ugettext_lazy as _
-from django.template.response import TemplateResponse
 
-
+from .. import response
 from .. import views
 from ..conf import settings
-
-
-class TemplateErrorResponseNotFound(TemplateResponse):
-    status_code = 404
-
-
-class TemplateErrorResponseForbidden(TemplateResponse):
-    status_code = 403
-
-
-class TemplateErrorResponseBadRequest(TemplateResponse):
-    status_code = 400
 
 
 urlpatterns = patterns('',
                        url(r'^/$',
                            views.DomainErrorView.as_view(
-                               response_class=TemplateErrorResponseBadRequest),
+                               response_class=response.TemplateErrorResponseBadRequest,
+                               template_name=settings.IKARI_ERROR_TEMPLATENAME),
                            settings.IKARI_ERRORCONTEXT_UNKNOWN,
                            name=settings.IKARI_URL_ERROR_UNKNOWN),
                        url(r'^inactive/$',
                            views.DomainErrorView.as_view(
-                               response_class=TemplateErrorResponseBadRequest),
+                               response_class=response.TemplateErrorResponseBadRequest,
+                               template_name=settings.IKARI_ERROR_TEMPLATENAME),
                            settings.IKARI_ERRORCONTEXT_INACTIVE,
                            name=settings.IKARI_URL_ERROR_INACTIVE),
                        url(r'^missing/$',
                            views.DomainErrorView.as_view(
-                               response_class=TemplateErrorResponseNotFound),
+                               response_class=response.TemplateErrorResponseNotFound,
+                               template_name=settings.IKARI_ERROR_TEMPLATENAME),
                            settings.IKARI_ERRORCONTEXT_INVALID,
                            name=settings.IKARI_URL_ERROR_DOESNTEXIST),
                        url(r'^private/$',
                            views.DomainErrorView.as_view(
-                               response_class=TemplateErrorResponseForbidden),
+                               response_class=response.TemplateErrorResponseForbidden,
+                               template_name=settings.IKARI_ERROR_TEMPLATENAME),
                            settings.IKARI_ERRORCONTEXT_PRIVATE,
                            name=settings.IKARI_URL_ERROR_PRIVATE),
                        )
